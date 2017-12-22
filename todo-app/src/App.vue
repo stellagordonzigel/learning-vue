7bc6971f38c5be60478aa9480dc594aec516650d
Stella <sgordonzigel@gmail.com>
2017-12-22 15:21:04 -0500

    add createTodo component--complete button is not quite working

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:create-todo="createTodo"></create-todo>
   </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
 
 export default {
   components: {
     name: 'app',
     TodoList,
+    CreateTodo,
   },
 
   data() {
@@ -33,6 +36,11 @@ export default {
         done: false,
       }]
     }
+  },
+  methods: {
+    createTodo({title, project, done}) {
+      this.todos.push({title, project, done})
+    },
   },
 };
 </script>
