a0dec7d8f20e143048ec21a2f6ead185ecb87052
Stella <sgordonzigel@gmail.com>
2017-12-22 12:24:36 -0500

    add component data

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <todo-list></todo-list>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -9,7 +9,30 @@ import TodoList from './components/TodoList';
 
 export default {
   components: {
+    name: 'app',
     TodoList,
+  },
+
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      } , {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }]
+    }
   },
 };
 </script>
--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -11,7 +11,7 @@
 <script type = "text/javascript" >
 
 export default {
-
+  props: ['todos']
 } ;
 
 </script>
